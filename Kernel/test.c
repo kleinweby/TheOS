962d3c54b6fe6def788daabe5541d5dfc9066c83
Christian Speich <christian@spei.ch>
2012-12-23 15:31:38 +0100

    Add LinkerHelper for variables declared by the linker. And a little c program to test if it compiles for now.

---
+++ Kernel/test.c
@@ -0,0 +1,19 @@
+
+#include "linkerHelper.h"
+
+LINKER_SYMBOL(KernelOffset);
+LINKER_SYMBOL(KernelLength);
+
+LINKER_SYMBOL(KernelStartupOffset);
+LINKER_SYMBOL(KernelStartupLength);
+
+LINKER_SYMBOL(KernelTextOffset);
+LINKER_SYMBOL(KernelTextLength);
+
+LINKER_SYMBOL(KernelDataOffset);
+LINKER_SYMBOL(KernelDataLength);
+
+void kmain()
+{
+	
+}
