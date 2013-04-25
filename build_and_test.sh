3d4f3a0ee0ec34c015b3166cd148b077835618cc
Christian Speich <christian@spei.ch>
2013-04-25 10:23:47 +0200

    Added ci test script. Disabled variable array length warining.

---
+++ build_and_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Find base dir
+pushd `dirname $0` > /dev/null
+BASEDIR=`pwd`
+popd > /dev/null
+
+pushd "$BASEDIR"
+pushd "CoreSystem"
+rake CoreSystem.framework || exit 1
+popd
+pushd "Kernel"
+rake || exit 1
+popd
+popd
